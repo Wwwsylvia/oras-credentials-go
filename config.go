fd276de2690629a3b503e8854380b0d9a2ac80c0
Sylvia Lei <lixlei@microsoft.com>
2023-03-17 20:31:14 +0800

    config opts
    Signed-off-by: Sylvia Lei <lixlei@microsoft.com>

--- config.go
+++ config.go
@@ -1,7 +1,12 @@
 package credentials
 
+type GetStoreOptions struct {
+	CredFilePath         string
+	DisablePlainTextSave bool
+}
+
 // GetConfiguredStore returns a new store from the settings in the configuration
 // file.
-func GetConfiguredStore(configPath, serverAddress string) (Store, error) {
+func GetConfiguredStore(configPath, serverAddress string, opts GetStoreOptions) (Store, error) {
 	panic("not implemented") // TODO: Implement
 }
